b990592085b5c144fb36b2fa50ea613fe35cadc1
homeworkfirst <zookotelif@gmail.com>
2024-09-04 22:02:06 +0300

    I done

--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -8,7 +8,6 @@ import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
